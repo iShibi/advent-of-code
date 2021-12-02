d01cd554b69ee13f9a8abf8e0b86aa11cc463780
Shubham Parihar <shubhamparihar391@gmail.com>
2021-12-02 12:00:47 +0530

    feat(2021/Day-2): add solution to Part-1

---
+++ 2021/Day-2/Part-1.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+int main() {
+  std::string direction;
+  int horizontal_position = 0, depth = 0; 
+  std::ifstream input_file("input.txt");
+  if (input_file.is_open()) {
+    while (std::getline(input_file, direction)) {
+      char direction_starts_with = direction.front();
+      int distance = direction.back() - 48;   // get the real integer value from the ASCII value
+      if (direction_starts_with == 'f') {
+        horizontal_position += distance;
+      } else if (direction_starts_with == 'u') {
+        depth -= distance;
+      } else if (direction_starts_with == 'd') {
+        depth += distance;
+      }
+    }
+    input_file.close();
+  } else {
+    std::cout << "Unable to open the file";
+  }
+  std::cout << horizontal_position * depth << std::endl;
+  return 0;
+}
