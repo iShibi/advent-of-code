e7b897e5690db82505e99e769ee24be1e2c58bd9
Shubham Parihar <shubhamparihar391@gmail.com>
2025-07-19 22:23:13 +0530

    feat: add solutions for y_2015 day_4

---
+++ src/y_2015/day_4/part_2.rs
@@ -0,0 +1,16 @@
+use md5;
+
+pub fn solve() -> usize {
+	let input_text = "yzbqklnj".to_string();
+	let mut num = 0;
+	let mut hash;
+	loop {
+		num += 1;
+		let key = format!("{}{}", input_text, num);
+		hash = format!("{:x}", md5::compute(key));
+		if hash.starts_with("000000") {
+			break;
+		}
+	}
+	num
+}
--- src/y_2015/mod.rs
+++ src/y_2015/mod.rs
@@ -1,2 +1,5 @@
 pub mod day_1;
 pub mod day_2;
+pub mod day_4;
+pub mod day_5;
+pub mod day_7;
