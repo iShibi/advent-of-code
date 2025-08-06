ec7857e3f15bd04ef071b4626830da17a56f0e41
Shubham Parihar <shubhamparihar391@gmail.com>
2025-08-06 16:50:17 +0530

    feat: add solution for y_2024 day_3 part_2

---
+++ src/y_2024/day_3/part_2.rs
@@ -0,0 +1,48 @@
+pub fn solve(input_text: &String) -> usize {
+	let do_blocks = input_text
+		.split("do()")
+		.map(|block| block.split("don't()").take(1).collect::<String>())
+		.collect::<Vec<_>>();
+	let mut result = 0;
+	for block in do_blocks {
+		let mut instruction = String::new();
+		let mut is_currently_parsing_mul = false;
+		for ch in block.chars() {
+			match ch {
+				'm' => {
+					if is_currently_parsing_mul {
+						instruction = String::new();
+					} else {
+						is_currently_parsing_mul = true;
+					}
+					instruction.push(ch);
+				}
+				')' => {
+					if is_currently_parsing_mul {
+						instruction.push(ch);
+						is_currently_parsing_mul = false;
+						if instruction.starts_with("mul(") {
+							let operands = instruction
+								.split(|ch| ch == '(' || ch == ',' || ch == ')')
+								.filter_map(|token| token.parse::<usize>().ok())
+								.collect::<Vec<_>>();
+							if operands.len() == 2 {
+								result += operands[0] * operands[1];
+							}
+						}
+						instruction = String::new();
+					}
+				}
+				_ => {
+					if is_currently_parsing_mul && (['m', 'u', 'l', '(', ','].contains(&ch) || ch.is_digit(10)) {
+						instruction.push(ch);
+					} else {
+						is_currently_parsing_mul = false;
+						instruction = String::new();
+					}
+				}
+			}
+		}
+	}
+	result
+}
